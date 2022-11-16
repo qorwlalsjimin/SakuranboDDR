4bdb003211ccd6165cf28ad0f1f0475059376506
지민 백 <s2104@e-mirim.hs.kr>
2022-11-16 16:32:19 +0900

    초기 커밋.

---
+++ SakuranboDDR/game.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// 헤더파일 중복 방지
+#ifndef GAME_H_
+#define GAME_H_
+
+#endif
---
+++ SakuranboDDR/main.cpp
@@ -0,0 +1,8 @@
+#include "game.h"
+
+int main() {
+	int WINDOW_WIDTH = 1200;
+	int WINDOW_HEIGHT = 800;
+
+	return 0;
+}
